03bc434071721e10410dcc24f5d4123cffe4e792
ivangorbunoff <60912904+ivangorbunoff@users.noreply.github.com>
2020-03-30 02:27:17 +0700

    module

---
+++ ivan.cprop/include.php
@@ -0,0 +1,5 @@
+<?php
+
+\Bitrix\Main\Loader::registerAutoLoadClasses('ivan.cprop', [
+    'CIBlockPropertyCPropAndEditor' => 'lib/CIBlockPropertyCPropAndEditor.php',
+]);
---
+++ ivan.cprop/install/version.php
@@ -0,0 +1,5 @@
+<?php
+$arModuleVersion = array(
+    'VERSION' => '1.0.1',
+    'VERSION_DATE' => '2020-03-30 16:51:00'
+);
